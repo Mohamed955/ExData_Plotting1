300dfd342d7a0c197f30f586a585117cedec6d01
Mohamed955 <30730551+Mohamed955@users.noreply.github.com>
2017-09-15 02:08:36 +0200

    Add files via upload

--- plot 1.R
+++ plot 1.R
@@ -1,13 +1,15 @@
+# importing files 
 data = read.table("household_power_consumption.txt" , sep = ";",header = TRUE)
-
+# subsetting data and changing into datetime category 
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 data1 = subset(data, data$Date >= "2007-02-01" & data$Date <= "2007-02-02")
 datetime <- paste(as.Date(data1$Date), data1$Time)
 data1$Datetime <- as.POSIXct(datetime)
 
+ # changing into numeric
 data1$Global_active_power = as.character(data1$Global_active_power)
 data1$Global_active_power= as.numeric(data1$Global_active_power)
-
+ # plotting 
 png(file="plot1.png", height=480, width=480)
 hist(data1$Global_active_power,col = "red" , main = "Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency")
 dev.off()
--- plot 4.R
+++ plot 4.R
@@ -1,23 +1,28 @@
+# importing files
 data = read.table("household_power_consumption.txt" , sep = ";",header = TRUE)
 
+# subsetting data and changing into datetime category 
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 data1 = subset(data, data$Date >= "2007-02-01" & data$Date <= "2007-02-02")
 datetime <- paste(as.Date(data1$Date), data1$Time)
 data1$Datetime <- as.POSIXct(datetime)
 
+# plotting
 png(file="plot4.png", height=480, width=480)
 
 par(mfrow=c(2,2),mar=c(4,4,2,1),oma=c(0,0,2,0))
 
-
+#plot1
 data1$Global_active_power = as.character(data1$Global_active_power)
 data1$Global_active_power= as.numeric(data1$Global_active_power)
 plot(data1$Global_active_power~data1$Datetime ,type="l", xlab="", ylab="Global Active Power (kilowatts)")
 
+#PLOT2
 data1$Voltage = as.character(data1$Voltage)
 data1$Voltage = as.numeric(data1$Voltage)
 plot(data1$Voltage~data1$Datetime,ylab="Voltage", xlab="Datetime",type="l")
 
+#PLOT3
 data1$Sub_metering_1 = as.character(data1$Sub_metering_1)
 data1$Sub_metering_1 = as.numeric(data1$Sub_metering_1)
 data1$Sub_metering_2 = as.character(data1$Sub_metering_2)
@@ -31,6 +36,7 @@ lines(data1$Datetime ,data1$Sub_metering_2 , col="red",type="l")
 lines(data1$Datetime ,data1$Sub_metering_3 , col="blue",type="l")
 legend("topright" , col=c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty = c(1,1,1))
 
+#PLOT4
 data1$Global_reactive_power= as.character(data1$Global_reactive_power)
 data1$Global_reactive_power= as.numeric(data1$Global_reactive_power)
 plot( data1$Global_reactive_power ~ data1$Datetime ,ylab="Global Rective Power",xlab="datetime",type="l")
